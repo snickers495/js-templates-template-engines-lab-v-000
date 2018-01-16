d3c61b3b1b1ab83b63c71037dbe48564eb95c727
Lisa Lee <snickers495@gmail.com>
2018-01-16 18:26:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,5 +22,12 @@ function createPost() {
 }
 
 function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commenterText").value;
 
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+  var commentTemplateFn = _.template(commentTemplate);
+  var commentHTML = commentTemplateFn({ 'commenterName': commenter, 'commenterText': comment });
+  var commentsDiv = document.getElementById("comments-template");
+  commentsDiv.innerHTML += commentHTML;
 }
