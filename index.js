5f2e88ff67c424ec672ef8cf9958374bba014f46
Lisa Lee <snickers495@gmail.com>
2018-01-16 18:12:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function createPost(){
+function createPost() {
+
+  ////Blog Values///
   var postTitle = document.getElementById("postTitle").value;
   var postAuthor = document.getElementById("postAuthor").value
   var postText = document.getElementById("postText").value;
+
+  ////Template/////
   var postTemplate = document.getElementById("post-template").innerHTML;
-  var templateFn = _.template(postTemplate);
+  var postTemplateFn = _.template(postTemplate);
+  var pageTemplate = document.getElementById("post-template").innerHTML;
+  var pageTemplateFn = _.template(pageTemplate);
+  
   var postDiv = document.getElementById("post");
   var templateHTML = templateFn({ 'postTitle': postTitle, 'postAuthor': postAuthor; 'postText': postText });
   postDiv.innerHTML += templateHTML;
